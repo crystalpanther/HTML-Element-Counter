a1b1af1a7309630a0e5d7d93c44ead7f44d956b7
Elina <elina.lilova@gmail.com>
2018-01-02 18:54:35 +0300

    wrote URL function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 /**
  * Created by elina on 1/2/2018.
  */
-window.onload = function () {
     function countElement (elem) {
         var result;
         result = elem.val();
-        return elem;
+        return result;
     }
-}
+    function loadURL() {
+        var tag = $('#htmlElement');
+        var window = $('.window');
+        var x = countElement(tag);
+        $.ajax({
+            url: 'page.html',
+            cache: false,
+            success: function(data) {
+                var element = $('<' + x + '>');
+                element.html(data);
+                var length = $('' + x, element).length;
+                window.text('Total' + ' ' + tag.val() + ' ' + 'on the page is: ' + length);
+            }
+        });
+    }
