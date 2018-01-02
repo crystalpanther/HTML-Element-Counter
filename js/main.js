1214203009f1bcbacddafa1a833b5cd320a960c7
Elina <elina.lilova@gmail.com>
2018-01-02 18:13:49 +0300

    added main.js file and wrote a function for taking from the input value as an argument. The function will return argument value

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 /**
  * Created by elina on 1/2/2018.
  */
+window.onload = function () {
+    function countElement (elem) {
+        var result;
+        result = elem.val();
+        return elem;
+    }
+}
