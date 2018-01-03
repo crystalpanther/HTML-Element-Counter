6da64ab8cf724b8fd9000b097fef07d4747a1e8a
Elina <elina.lilova@gmail.com>
2018-01-03 17:31:28 +0300

    wrote the script that counting time request

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 /**
  * Created by elina on 1/2/2018.
  */
-    function countElement (elem) {
-        var result;
-        result = elem.val(); //take an input with "htmlElement" id
-        return result; //return value
-    }
-    function loadURL() {
+var a = Date.now(); // start time for all scripts
+var res;
+function countElement (elem) {
+    var result;
+    result = elem.val(); //take an input with "htmlElement" id
+    return result; //return value
+}
+function loadURL() {
         var tag = $('#htmlElement');
         var url = 'page.html';
         var window = $('.window');
@@ -19,6 +21,7 @@
             crossDomain: true,
             success: function(data) {
                 var element = $('<' + x + '>'); //take an input value and
+
                 var d = new Date(); //create a new object for take current time
                 var curr_date = d.getDate();
 
@@ -38,11 +41,15 @@
 
                 var curr_month = month[d.getMonth()];
                 var curr_year = d.getFullYear();
+                var time = d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
+
+                var timeSec = res;
+
                 var pages = element.html(data);
                 var length = $(x, element).length;
                 window.text(
                     'URL' + ' ' + url + ' ' + 'Fetched on' + ' ' +
-                    curr_date + ' ' + curr_month + ' ' + curr_year + ', ' + 'took' + ' ' + timeSec + '. ' +
+                    curr_date + ' ' + curr_month + ' ' + curr_year + ' ' + time + ', ' + 'took' + ' ' + timeSec + ' msec. ' +
                     'Element' + ' ' + tag.val() + ' ' + 'appeared ' + length + ' ' + 'times in page.'
                 );
             },
@@ -51,3 +58,9 @@
             }
         });
     }
+var ok = [];
+for(var i = 0; i < 1e+5/*100000*/;i++) ok.push(i);
+var b = Date.now(); //end time for all scripts
+
+res = b - a; // counting time that take a request
+console.log(res);
